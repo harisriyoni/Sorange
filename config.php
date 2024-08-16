dae1c6cc9af176b696802d0043e6291017f0f9b8
harisriyoni <1214047@std.ac.id>
2024-08-17 03:59:39 +0700

    deploy

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 // Informasi koneksi database
-$server = "nuskkyrsgmn5rw8c.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+$server = "mysql://semay6vxedd4p6sx:thh3vo09kieb4dtr@nuskkyrsgmn5rw8c.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/fizefwddyw5zc87p";
 $user = "semay6vxedd4p6sx";
 $password = "thh3vo09kieb4dtr";
 $namadb = "fizefwddyw5zc87p";
