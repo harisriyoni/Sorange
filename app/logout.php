8af184ead8b7a0d8932651a0607819efe70e7bb3
harisriyoni <1214047@std.ac.id>
2024-08-17 22:23:42 +0700

    fix all

--- app/logout.php
+++ app/logout.php
@@ -1,5 +1,18 @@
 <?php
 session_start();
+
+// Hapus semua variabel sesi
+session_unset();
+
+// Hancurkan sesi
 session_destroy();
-header("location:login.php");
+
+// Hapus cookie PHPSESSID
+if (isset($_COOKIE['PHPSESSID'])) {
+    setcookie('PHPSESSID', '', time() - 3600, '/'); // Atur waktu kedaluwarsa di masa lalu untuk menghapus cookie
+}
+
+// Arahkan kembali ke halaman login
+header("Location: login.php");
+exit();
 ?>
