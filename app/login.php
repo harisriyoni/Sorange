f06d1080a4855e5a9b12f5c77954b1ce557b5167
harisriyoni <1214047@std.ac.id>
2024-08-17 08:07:14 +0700

    feat:page login sweet alert

--- app/login.php
+++ app/login.php
@@ -15,7 +15,7 @@ if (isset($_POST['login'])) {
     $password = $_POST['password'];
 
     if ($username == ''or$password == '') {
-        $err .= "Silakan masukkan username dan password";
+        $err .= "Silakan masukkan username dan password<br>";
     }
 
     if (empty($err)) {
@@ -24,7 +24,7 @@ if (isset($_POST['login'])) {
         $r1 = mysqli_fetch_array($q1);
 
         if (!$r1 || $r1['password'] != md5($password)) {
-            $err .= "Anda Belum Daftar atau Password salah";
+            $err .= "Anda Belum Daftar atau Password salah<br>";
         }
     }
 
@@ -32,8 +32,6 @@ if (isset($_POST['login'])) {
         $_SESSION['admin_username'] = $username;
         header("location:index.php");
         exit();
-    } else {
-        // Send error messages to JavaScriptecho"<script>var errorMessage = '".addslashes($err)."';</script>";
     }
 }
 ?>
@@ -44,42 +42,43 @@ if (isset($_POST['login'])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>login</title>
+    <title>Login</title>
     <script src="https://cdn.tailwindcss.com"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.all.min.js"></script>
     <script>
-        <?php 
-        if (!empty($err)) { ?>
-            Swal.fire({
-                icon: 'error',
-                title: 'Oops...',
-                html: '<?php echo $err; ?>'
-            });
-        <?php } ?>
+        document.addEventListener('DOMContentLoaded', function() {
+            <?php
+                if (!empty($err)) { ?>
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Oops...',
+                    html: '<?php echo addslashes($err); ?>'
+                });
+            <?php } ?>
+        });
     </script>
 </head>
 
 <body>
-
     <section class="bg-gray-50 dark:bg-gray-900">
-        <div id="app" class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-            <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-                <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
-                    <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-                        Masuk Dashboard
+        <divid="app" class="flexflex-colitems-centerjustify-centerpx-6 py-8 mx-automd:h-screenlg:py-0">
+            <divclass="w-fullbg-whiterounded-lgshadowdark:bordermd:mt-0 sm:max-w-mdxl:p-0 dark:bg-gray-800 dark:border-gray-700">
+                <divclass="p-6 space-y-4 md:space-y-6 sm:p-8">
+                    <h1class="text-xlfont-boldleading-tighttracking-tighttext-gray-900 md:text-2xldark:text-white">
+                        MasukDashboard
                     </h1>
-                    <form class="space-y-4 md:space-y-6" action="" method="POST">
+                    <formclass="space-y-4 md:space-y-6" action="" method="POST">
                         <div>
-                            <label for="username" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Username</label>
-                            <input type="username" value="<?php echo $username ?>" name="username" id="username" class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 input" placeholder="username" required="">
+                            <labelfor="username" class="blockmb-2 text-smfont-mediumtext-gray-900 dark:text-white">Username</label>
+                            <inputtype="text" value="<?php echo htmlspecialchars($username); ?>" name="username" id="username" class="bg-gray-50 borderborder-gray-300 text-gray-900 rounded-lgfocus:ring-primary-600 focus:border-primary-600 blockw-fullp-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-whitedark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="username" required>
                         </div>
                         <div>
-                            <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                            <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="">
+                            <labelfor="password" class="blockmb-2 text-smfont-mediumtext-gray-900 dark:text-white">Password</label>
+                            <inputtype="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 borderborder-gray-300 text-gray-900 rounded-lgfocus:ring-primary-600 focus:border-primary-600 blockw-fullp-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-whitedark:focus:ring-blue-500 dark:focus:border-blue-500" required>
                         </div>
-                        <button type="submit" name="login" value="LOGIN" class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Masuk</button>
-                        <p class="text-sm font-light text-gray-500 dark:text-gray-400">
-                            kamu belum punya akun? <a href="registrasi.php" class="font-medium text-primary-600 hover:underline dark:text-primary-500">Daftar disini</a>
+                        <buttontype="submit" name="login" value="LOGIN" class="w-fulltext-whitebg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-nonefocus:ring-primary-300 font-mediumrounded-lgtext-smpx-5 py-2.5 text-centerdark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Masuk</button>
+                        <pclass="text-smfont-lighttext-gray-500 dark:text-gray-400">
+                            Kamubelumpunyaakun? <ahref="registrasi.php" class="font-mediumtext-primary-600 hover:underlinedark:text-primary-500">Daftardisini</a>
                         </p>
                     </form>
                 </div>
