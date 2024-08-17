721a8dea43ad1b110b7416d17df64f9143175e2b
harisriyoni <1214047@std.ac.id>
2024-08-17 07:15:07 +0700

    deploy

--- app/login.php
+++ app/login.php
@@ -36,27 +36,35 @@ if(isset($_SESSION['admin_username'])){
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>login</title>
-    <link rel="stylesheet" href="login_.css">
+    <!-- <link rel="stylesheet" href="login_.css"> -->
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body>
-    <div id="app" ALIGN="CENTER">
-        <h1>SORANGE.ID | BERITA MASA KINI</h1>
-        <h1>Halaman Login</h1>
-        <?php 
-        if($err){
-            echo "<ul>$err</ul>";
-        }
-        ?>
-        <form action="" method="POST">
-            <input type="text" value="<?php echo $username?>" name="username" class="input" placeholder="username"/><br><br>
-            <input type="password" name="password" class="input" placeholder="isikan password"/><br><br>
-            <input type="submit" name="login" value="LOGIN"/><br><br>
-            <a href="registrasi.php">Buat Akun</a>
-           
-        </form>
-     
-        </div>
-
-    </div>
+   
+    <section class="bg-gray-50 dark:bg-gray-900">
+  <div id="app" class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+      <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+          <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
+              <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                  Masuk Dashboard
+              </h1>
+              <form class="space-y-4 md:space-y-6" action="" method="POST">
+                  <div>
+                      <label for="username" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Username</label>
+                      <input type="username" value="<?php echo $username?>" name="username" id="username" class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 input" placeholder="username" required="">
+                  </div>
+                  <div>
+                      <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                      <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="">
+                  </div>
+                  <button type="submit" class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</button>
+                  <p class="text-sm font-light text-gray-500 dark:text-gray-400" type="submit" name="login" value="LOGIN">
+                      Don’t have an account yet? <a href="registrasi.php" class="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up</a>
+                  </p>
+              </form>
+          </div>
+      </div>
+  </div>
+</section>
 </body>
 </html>
