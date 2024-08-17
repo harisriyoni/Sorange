cd9a3358fe3c0a314d841395b399830f196eb04e
harisriyoni <1214047@std.ac.id>
2024-08-17 07:27:59 +0700

    feat:page registrasi

--- app/registrasi.php
+++ app/registrasi.php
@@ -17,7 +17,7 @@ if(isset($_POST['register'])){
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Halaman Registrasi</title>
-    <link rel="stylesheet" href="login_.css">
+    <script src="https://cdn.tailwindcss.com"></script>
     <style>
         label {
             display: block;
