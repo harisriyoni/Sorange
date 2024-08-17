a2f774f3d9c2151dd044b34b6595d5742d532538
harisriyoni <1214047@std.ac.id>
2024-08-17 09:15:08 +0700

    feat:home page

--- app/index.php
+++ app/index.php
@@ -131,30 +131,16 @@ include("config.php");
     <aside class="w-full md:w-1/3 flex flex-col items-center px-3">
 
       <div class="w-full bg-white shadow flex flex-col my-4 p-6">
-        <p class="text-xl font-semibold pb-5">About Us</p>
-        <p class="pb-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas mattis est eu odio sagittis tristique. Vestibulum ut finibus leo. In hac habitasse platea dictumst.</p>
+        <p class="text-xl font-semibold pb-5">Tentang Kami</p>
+        <p class="pb-2">Selamat datang di Perusahaan Sorange, portal berita terdepan yang menyediakan informasi terbaru dan terpercaya. Kami berkomitmen untuk memberikan berita terkini dengan akurasi tinggi dan kecepatan penyampaian yang optimal.
+Di Perusahaan Sorange, kami percaya bahwa informasi yang tepat waktu dan akurat adalah kunci untuk memahami dunia di sekitar kita. Kami menghadirkan berbagai topik berita, mulai dari berita lokal hingga internasional, dengan pendekatan jurnalistik yang profesional dan objektif.
+Selain itu, kami juga membuka kesempatan bagi Anda untuk menjadi bagian dari komunitas kami dengan memposting berita terbaru. Kami menyambut setiap kontribusi dari pembaca kami yang ingin berbagi informasi penting dan relevan dengan masyarakat.
+Bergabunglah dengan kami di Perusahaan Sorange, dan jadilah bagian dari revolusi informasi yang memberikan dampak positif bagi masyarakat. Kami berkomitmen untuk terus berkembang dan beradaptasi dengan kebutuhan berita terkini untuk memenuhi harapan Anda.
+Terima kasih telah mengunjungi kami, dan selamat membaca!</p>
         <a href="#" class="w-full bg-blue-800 text-white font-bold text-sm uppercase rounded hover:bg-blue-700 flex items-center justify-center px-2 py-3 mt-4">
-          Get to know us
+          Bergabung Bersama Kami!
         </a>
-      </div>
-
-      <div class="w-full bg-white shadow flex flex-col my-4 p-6">
-        <p class="text-xl font-semibold pb-5">Instagram</p>
-        <div class="grid grid-cols-3 gap-3">
-          <img class="hover:opacity-75" src="https://source.unsplash.com/collection/1346951/150x150?sig=1">
-          <img class="hover:opacity-75" src="https://source.unsplash.com/collection/1346951/150x150?sig=2">
-          <img class="hover:opacity-75" src="https://source.unsplash.com/collection/1346951/150x150?sig=3">
-          <img class="hover:opacity-75" src="https://source.unsplash.com/collection/1346951/150x150?sig=4">
-          <img class="hover:opacity-75" src="https://source.unsplash.com/collection/1346951/150x150?sig=5">
-          <img class="hover:opacity-75" src="https://source.unsplash.com/collection/1346951/150x150?sig=6">
-          <img class="hover:opacity-75" src="https://source.unsplash.com/collection/1346951/150x150?sig=7">
-          <img class="hover:opacity-75" src="https://source.unsplash.com/collection/1346951/150x150?sig=8">
-          <img class="hover:opacity-75" src="https://source.unsplash.com/collection/1346951/150x150?sig=9">
-        </div>
-        <a href="#" class="w-full bg-blue-800 text-white font-bold text-sm uppercase rounded hover:bg-blue-700 flex items-center justify-center px-2 py-3 mt-6">
-          <i class="fab fa-instagram mr-2"></i> Follow @dgrzyb
-        </a>
-      </div>
+      </div>  
 
     </aside>
 
