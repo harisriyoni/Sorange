c3b118f35b1a10a5f4340a935c2f4cfa9e9bb911
harisriyoni <1214047@std.ac.id>
2024-08-17 09:04:08 +0700

    feat:page login sweet alert

--- app/index.php
+++ app/index.php
@@ -1,87 +1,243 @@
 <?php
-session_start();
-include ("config.php");
-if(!isset($_SESSION['admin_username'])){
-    header("location:login.php");
-}
+include("config.php");
 ?>
-<!doctype html>
+
+<!DOCTYPE html>
 <html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link rel="stylesheet" href="style.css">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>SORANGE</title>
+  <meta name="author" content="David Grzyb">
+  <meta name="description" content="">
 
-    <title>SORANGE.ID</title>
-  </head>
-  <body>
-  <nav>
-        <div class="logo">
-            <h4>SORANGE.ID</h4>
-        </div>
-        <div class="dropdown">
-  <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
-    Berita
-  </button>
-  <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-    <li><a class="dropdown-item" href="index.php">Home</a></li>
-    <li><a class="dropdown-item" href="artikel-edukasi.php">Baca Berita</a></li>
-  </ul>
-</div>
-          <div class="dropdown">
-  <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
-    Data Berita/Logout
-  </button>
-  <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-    <li><a class="dropdown-item" href="form-create.php">Upload Berita</a></li>
-    <li><a class="dropdown-item" href="list-kategori.php">Data Kategori Berita</a></li>
-    <li><a class="dropdown-item" href="logout.php">Logout</a></li>
-  </ul>
-</div>
+  <!-- Tailwind -->
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.2.19/tailwind.min.css" rel="stylesheet">
+  <style>
+    @import url('https://fonts.googleapis.com/css?family=Karla:400,700&display=swap');
+
+    .font-family-karla {
+      font-family: karla;
+    }
+  </style>
+
+  <!-- AlpineJS -->
+  <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
+  <!-- Font Awesome -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/js/all.min.js" integrity="sha256-KzZiKy0DWYsnwMF+X1DvQngQ2/FxF7MF3Ff72XcpuPs=" crossorigin="anonymous"></script>
+</head>
+
+<body class="bg-white font-family-karla">
+
+  <!-- Top Bar Nav -->
+  <nav class="w-full py-4 bg-blue-800 shadow">
+    <div class="w-full container mx-auto flex flex-wrap items-center justify-between">
+
+      <nav>
+        <ul class="flex items-center justify-between font-bold text-sm text-white uppercase no-underline">
+          <li><a class="hover:text-gray-200 hover:underline px-4" href="">Mau Post Berita Kamu? Daftar Sekarang Gratis!</a></li>
+          <li><a class="hover:text-gray-200 hover:underline px-4" href="login.php">Masuk</a></li>
+          <li><a class="hover:text-gray-200 hover:underline px-4" href="registrasi.php">Registrasi</a></li>
+          <li><a class="hover:text-gray-200 hover:underline px-4" href="registrasi.php">Logout</a></li>
+        </ul>
       </nav>
-      <script src="js/ss.js"></script>
+
+      <div class="flex items-center text-lg no-underline text-white pr-6">
+        <a class="" href="#">
+          <i class="fab fa-facebook"></i>
+        </a>
+        <a class="pl-6" href="#">
+          <i class="fab fa-instagram"></i>
+        </a>
+        <a class="pl-6" href="#">
+          <i class="fab fa-twitter"></i>
+        </a>
+        <a class="pl-6" href="#">
+          <i class="fab fa-linkedin"></i>
+        </a>
+      </div>
+    </div>
+
+  </nav>
+
+  <!-- Text Header -->
+  <header class="w-full container mx-auto">
+    <div class="flex flex-col items-center py-12">
+      <a class="font-bold text-gray-800 uppercase hover:text-gray-700 text-5xl" href="#">
+        SELAMAT DATANG SORANGE
+      </a>
+      <p class="text-lg text-gray-600">
+        Dimana Kamu bisa Melihat Berita Update Terkini!
+      <p class="text-lg text-gray-600">
+        Kamu bisa Post Berita Kamu di Website Kami, Segera Bergabung!
+      </p>
+      </p>
+    </div>
+  </header>
+
+  <!-- Topic Nav -->
+  <nav class="w-full py-4 border-t border-b bg-gray-100" x-data="{ open: false }">
+    <div class="block sm:hidden">
+      <a
+        href="#"
+        class="block md:hidden text-base font-bold uppercase text-center flex justify-center items-center"
+        @click="open = !open">
+        Topics <i :class="open ? 'fa-chevron-down': 'fa-chevron-up'" class="fas ml-2"></i>
+      </a>
+    </div>
+    <div :class="open ? 'block': 'hidden'" class="w-full flex-grow sm:flex sm:items-center sm:w-auto">
+      <div class="w-full container mx-auto flex flex-col sm:flex-row items-center justify-center text-sm font-bold uppercase mt-0 px-6 py-2">
+        <a href="#" class="hover:bg-gray-400 rounded py-2 px-4 mx-2">Technology</a>
+        <a href="#" class="hover:bg-gray-400 rounded py-2 px-4 mx-2">Automotive</a>
+        <a href="#" class="hover:bg-gray-400 rounded py-2 px-4 mx-2">Finance</a>
+        <a href="#" class="hover:bg-gray-400 rounded py-2 px-4 mx-2">Politics</a>
+        <a href="#" class="hover:bg-gray-400 rounded py-2 px-4 mx-2">Culture</a>
+        <a href="#" class="hover:bg-gray-400 rounded py-2 px-4 mx-2">Sports</a>
+      </div>
+    </div>
+  </nav>
+
+
+  <div class="container mx-auto flex flex-wrap py-6">
+
+    <!-- Post Section -->
+    <section class="w-full md:w-2/3 flex flex-col items-center px-3">
+
+      <article class="flex flex-col shadow my-4">
+        <?php
+        $query = mysqli_query($db, "SELECT * FROM kategori_berita");
+        foreach ($query as $row) :
+        ?>
+          <!-- Article Image -->
+          <a href="#" class="hover:opacity-75">
+            <img src="<?php echo $row['gambar']; ?>" height="50" width="950">
+          </a>
+          <div class="bg-white flex flex-col justify-start p-6">
+            <a href="#" class="text-blue-700 text-sm font-bold uppercase pb-4"><?php echo $row['kategori']; ?></a>
+            <a href="#" class="text-3xl font-bold hover:text-gray-700 pb-4"><?php echo $row['judul_berita']; ?></a>
+            <p href="#" class="text-sm pb-8">
+              By <a href="#" class="font-semibold hover:text-gray-800">Admin</a> 2024
+            </p>
+            <h1 class="text-2xl font-bold pb-3"><?php echo $row['judul_berita']; ?></h1>
+            <p class="pb-3"><?php echo $row['isi_berita']; ?></p>
+          </div>
+        <?php endforeach; ?>
+      </article>
       
-   
- <div class="jumbotron">
-   <div class="container"><br>
-      <h1 class="display-4 text-white"><b> SORANGE </b><br> <span class="font-weight-bold text-white"><b> SELAMAT DATANG SORANGE</b></span></h1>
-     
-    <p class="lead text-white"><b>Selamat Membaca</b> </p>
-    <p class="lead text-white"><b>Upload Berita Kamu Di Bawah Sini!</b> </p>
-    <a class="btn btn-primary btn-lg font-weight-bold" href="form-create.php" role="button"><img src="upload2.gif" height="60" alt=""></a>
-   </div>
- </div> 
- <br><br><br><br><br>
-<!-- card baru -->
-	<div class="container-fluid">
-    <?php
-    $query = mysqli_query($db, "SELECT*FROM kategori_berita")?>
-      <div class="row">
-        <?php foreach ($query as $row) :?>
-            <div class="card" style="width: 27rem;" ALIGN="center">
-            <img src="<?php echo $row['gambar'] ;?>" class="card-img-top" alt="...">
-            <div class="card-body">
-            <h5 class="card-title"><?php echo $row['judul_berita'];?></h5>
-            <small><?php echo $row['deskripsi'];?></p></small><br>
-            <small><?php echo $row['kategori'];?></small>
-            <p class="card-text">Klik Selengkapnya untuk membaca</p>
-             <a href="artikel-edukasi.php" class="btn btn-primary">Selengkapnya</a>
-            </div>
-      </div><br>
-        <?php endforeach ?>                
-     </div>
-  </div>
-</div> 
-       <br><br><br>   
-    <!--FOOTER-->
-    <div class="footer-bottom">
-        <div class="container-fluid text-center">
-            <p class="text-white"> Developed by.<a class="text-white" href="index.php">Sorange.ID</a></p>
+      <div class="w-full flex flex-col text-center md:text-left md:flex-row shadow bg-white mt-10 mb-10 p-6">
+        <div class="w-full md:w-1/5 flex justify-center md:justify-start pb-4">
+          <img src="https://source.unsplash.com/collection/1346951/150x150?sig=1" class="rounded-full shadow h-32 w-32">
         </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-  </body>
+        <div class="flex-1 flex flex-col justify-center md:justify-start">
+          <p class="font-semibold text-2xl">David</p>
+          <p class="pt-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur vel neque non libero suscipit suscipit eu eu urna.</p>
+          <div class="flex items-center justify-center md:justify-start text-2xl no-underline text-blue-800 pt-4">
+            <a class="" href="#">
+              <i class="fab fa-facebook"></i>
+            </a>
+            <a class="pl-4" href="#">
+              <i class="fab fa-instagram"></i>
+            </a>
+            <a class="pl-4" href="#">
+              <i class="fab fa-twitter"></i>
+            </a>
+            <a class="pl-4" href="#">
+              <i class="fab fa-linkedin"></i>
+            </a>
+          </div>
+        </div>
+      </div>
+
+    </section>
+
+    <!-- Sidebar Section -->
+    <aside class="w-full md:w-1/3 flex flex-col items-center px-3">
+
+      <div class="w-full bg-white shadow flex flex-col my-4 p-6">
+        <p class="text-xl font-semibold pb-5">About Us</p>
+        <p class="pb-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas mattis est eu odio sagittis tristique. Vestibulum ut finibus leo. In hac habitasse platea dictumst.</p>
+        <a href="#" class="w-full bg-blue-800 text-white font-bold text-sm uppercase rounded hover:bg-blue-700 flex items-center justify-center px-2 py-3 mt-4">
+          Get to know us
+        </a>
+      </div>
+
+      <div class="w-full bg-white shadow flex flex-col my-4 p-6">
+        <p class="text-xl font-semibold pb-5">Instagram</p>
+        <div class="grid grid-cols-3 gap-3">
+          <img class="hover:opacity-75" src="https://source.unsplash.com/collection/1346951/150x150?sig=1">
+          <img class="hover:opacity-75" src="https://source.unsplash.com/collection/1346951/150x150?sig=2">
+          <img class="hover:opacity-75" src="https://source.unsplash.com/collection/1346951/150x150?sig=3">
+          <img class="hover:opacity-75" src="https://source.unsplash.com/collection/1346951/150x150?sig=4">
+          <img class="hover:opacity-75" src="https://source.unsplash.com/collection/1346951/150x150?sig=5">
+          <img class="hover:opacity-75" src="https://source.unsplash.com/collection/1346951/150x150?sig=6">
+          <img class="hover:opacity-75" src="https://source.unsplash.com/collection/1346951/150x150?sig=7">
+          <img class="hover:opacity-75" src="https://source.unsplash.com/collection/1346951/150x150?sig=8">
+          <img class="hover:opacity-75" src="https://source.unsplash.com/collection/1346951/150x150?sig=9">
+        </div>
+        <a href="#" class="w-full bg-blue-800 text-white font-bold text-sm uppercase rounded hover:bg-blue-700 flex items-center justify-center px-2 py-3 mt-6">
+          <i class="fab fa-instagram mr-2"></i> Follow @dgrzyb
+        </a>
+      </div>
+
+    </aside>
+
+  </div>
+
+  <footer class="w-full border-t bg-white pb-12">
+    <div
+      class="relative w-full flex items-center invisible md:visible md:pb-12"
+      x-data="getCarouselData()">
+      <button
+        class="absolute bg-blue-800 hover:bg-blue-700 text-white text-2xl font-bold hover:shadow rounded-full w-16 h-16 ml-12"
+        x-on:click="decrement()">
+        &#8592;
+      </button>
+      <template x-for="image in images.slice(currentIndex, currentIndex + 6)" :key="images.indexOf(image)">
+        <img class="w-1/6 hover:opacity-75" :src="image">
+      </template>
+      <button
+        class="absolute right-0 bg-blue-800 hover:bg-blue-700 text-white text-2xl font-bold hover:shadow rounded-full w-16 h-16 mr-12"
+        x-on:click="increment()">
+        &#8594;
+      </button>
+    </div>
+    <div class="w-full container mx-auto flex flex-col items-center">
+      <div class="flex flex-col md:flex-row text-center md:text-left md:justify-between py-6">
+        <a href="#" class="uppercase px-3">About Us</a>
+        <a href="#" class="uppercase px-3">Privacy Policy</a>
+        <a href="#" class="uppercase px-3">Terms & Conditions</a>
+        <a href="#" class="uppercase px-3">Contact Us</a>
+      </div>
+      <div class="uppercase pb-6">&copy; myblog.com</div>
+    </div>
+  </footer>
+
+  <script>
+    function getCarouselData() {
+      return {
+        currentIndex: 0,
+        images: [
+          'https://source.unsplash.com/collection/1346951/800x800?sig=1',
+          'https://source.unsplash.com/collection/1346951/800x800?sig=2',
+          'https://source.unsplash.com/collection/1346951/800x800?sig=3',
+          'https://source.unsplash.com/collection/1346951/800x800?sig=4',
+          'https://source.unsplash.com/collection/1346951/800x800?sig=5',
+          'https://source.unsplash.com/collection/1346951/800x800?sig=6',
+          'https://source.unsplash.com/collection/1346951/800x800?sig=7',
+          'https://source.unsplash.com/collection/1346951/800x800?sig=8',
+          'https://source.unsplash.com/collection/1346951/800x800?sig=9',
+        ],
+        increment() {
+          this.currentIndex = this.currentIndex === this.images.length - 6 ? 0 : this.currentIndex + 1;
+        },
+        decrement() {
+          this.currentIndex = this.currentIndex === this.images.length - 6 ? 0 : this.currentIndex - 1;
+        },
+      }
+    }
+  </script>
+
+</body>
+
 </html>
