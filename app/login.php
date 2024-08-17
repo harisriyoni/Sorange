b9afa6ded4be2e62640e638acd616fd7bb7c30ad
harisriyoni <1214047@std.ac.id>
2024-08-17 08:08:38 +0700

    feat:page login sweet alert

--- app/login.php
+++ app/login.php
@@ -61,24 +61,24 @@ if (isset($_POST['login'])) {
 
 <body>
     <section class="bg-gray-50 dark:bg-gray-900">
-        <divid="app" class="flexflex-colitems-centerjustify-centerpx-6 py-8 mx-automd:h-screenlg:py-0">
-            <divclass="w-fullbg-whiterounded-lgshadowdark:bordermd:mt-0 sm:max-w-mdxl:p-0 dark:bg-gray-800 dark:border-gray-700">
-                <divclass="p-6 space-y-4 md:space-y-6 sm:p-8">
-                    <h1class="text-xlfont-boldleading-tighttracking-tighttext-gray-900 md:text-2xldark:text-white">
-                        MasukDashboard
+        <div id="app" class="flexflex-colitems-centerjustify-centerpx-6 py-8 mx-automd:h-screenlg:py-0">
+            <div class="w-fullbg-whiterounded-lgshadowdark:bordermd:mt-0 sm:max-w-mdxl:p-0 dark:bg-gray-800 dark:border-gray-700">
+                <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
+                    <h1 class="text-xlfont-boldleading-tighttracking-tighttext-gray-900 md:text-2xldark:text-white">
+                        Masuk Dashboard
                     </h1>
                     <formclass="space-y-4 md:space-y-6" action="" method="POST">
                         <div>
                             <labelfor="username" class="blockmb-2 text-smfont-mediumtext-gray-900 dark:text-white">Username</label>
-                            <inputtype="text" value="<?php echo htmlspecialchars($username); ?>" name="username" id="username" class="bg-gray-50 borderborder-gray-300 text-gray-900 rounded-lgfocus:ring-primary-600 focus:border-primary-600 blockw-fullp-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-whitedark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="username" required>
+                            <input type="text" value="<?php echo htmlspecialchars($username); ?>" name="username" id="username" class="bg-gray-50 borderborder-gray-300 text-gray-900 rounded-lgfocus:ring-primary-600 focus:border-primary-600 blockw-fullp-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-whitedark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="username" required>
                         </div>
                         <div>
-                            <labelfor="password" class="blockmb-2 text-smfont-mediumtext-gray-900 dark:text-white">Password</label>
-                            <inputtype="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 borderborder-gray-300 text-gray-900 rounded-lgfocus:ring-primary-600 focus:border-primary-600 blockw-fullp-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-whitedark:focus:ring-blue-500 dark:focus:border-blue-500" required>
+                            <label for="password" class="blockmb-2 text-smfont-mediumtext-gray-900 dark:text-white">Password</label>
+                            <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 borderborder-gray-300 text-gray-900 rounded-lgfocus:ring-primary-600 focus:border-primary-600 blockw-fullp-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-whitedark:focus:ring-blue-500 dark:focus:border-blue-500" required>
                         </div>
-                        <buttontype="submit" name="login" value="LOGIN" class="w-fulltext-whitebg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-nonefocus:ring-primary-300 font-mediumrounded-lgtext-smpx-5 py-2.5 text-centerdark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Masuk</button>
-                        <pclass="text-smfont-lighttext-gray-500 dark:text-gray-400">
-                            Kamubelumpunyaakun? <ahref="registrasi.php" class="font-mediumtext-primary-600 hover:underlinedark:text-primary-500">Daftardisini</a>
+                        <button type="submit" name="login" value="LOGIN" class="w-fulltext-whitebg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-nonefocus:ring-primary-300 font-mediumrounded-lgtext-smpx-5 py-2.5 text-centerdark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Masuk</button>
+                        <p class="text-smfont-lighttext-gray-500 dark:text-gray-400">
+                            Kamu belum punya akun? <a href="registrasi.php" class="font-mediumtext-primary-600 hover:underlinedark:text-primary-500">Daftar Disini</a>
                         </p>
                     </form>
                 </div>
